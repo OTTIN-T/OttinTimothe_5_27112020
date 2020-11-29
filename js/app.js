20ebb9464fc64256554d961d7cef0cc1579ae776
OTTIN <ottin.timothe@gmail.com>
2020-11-29 17:44:48 +0100

    Liste lenses OK
    Liste déroulantes pour chaque caméra OK

--- js/app.js
+++ js/app.js
@@ -1,3 +1,8 @@
+//Appel URL
+const getUrlProduct = window.location.href //Obtiens l'URL actuel
+const urlProduct = new URL(getUrlProduct) //Permet d'obtenir les objet de l'url 
+const idCamera = urlProduct.searchParams.get("id")//On récupère .search de l'URL
+
 //Appel de notre API
 fetch("http://localhost:3000/api/cameras")
 .then((result) => {
@@ -5,7 +10,14 @@ fetch("http://localhost:3000/api/cameras")
     .then((cameras) => {
         const $cameraList = document.querySelector('#camera-list')
         cameras.forEach(camera => {
-        console.log("cameras", cameras)
+            const lenses = document.createElement("select")
+                let i = 0
+                for( i; i<camera.lenses.length; i++){
+                    const option = document.createElement("option")
+                    option.setAttribute("value", camera.lenses[i])                
+                    option.innerHTML = camera.lenses[i]
+                    lenses.appendChild(option)
+                }
             $cameraList.innerHTML += 
             `<div id="camera-card" class="card col-lg-5 col-md-12 col-sm-12 col-12 mt-3 mb-3 border-dark shadow">
                 <a href="../html/produit.html?id=${camera._id}">
@@ -19,42 +31,20 @@ fetch("http://localhost:3000/api/cameras")
                             <label class="input-group-text" for="inputGroupSelect01">Lentilles</label>
                         </div>
                         <select class="custom-select col-lg-6 col-md-6 col-sm-6 col-12" id="inputGroupSelect01">
-                            <option selected value="1">${camera.lenses[0]}</option>
-                            <option value="2">${camera.lenses[1]}</option>
+                            <option selected selected value="1">Sélectionner</option>
+                            ${lenses.innerHTML}
                         </select>
                         <p id="camera-price" class="card-text col-lg-6 col-md-6 col-sm-6 col-12 mx-auto">${camera.price + " €"}</p>
                     </div>  
                     <a id="camera-buy"class="btn btn-secondary col-6 mx-auto mt-3" href="#" role="button">Ajouter au panier</a>
                 </div>   
             </div>` 
-            console.log("camera", camera)
-        })
-        
-    })
-    
+        })    
+    }) 
 })
 .catch((error) => {
     console.log(error);
 })
-
-
-
-// //Test appel URL
-const getUrlProduct = window.location.href //on obtiens l'URL
-console.log("getUrlProduct", getUrlProduct)
-
-const urlProduct = new URL(getUrlProduct) 
-console.log("urlProduct", urlProduct)
-
-const idCamera = urlProduct.searchParams.get("id")
-console.log("idCamera", idCamera)
-// const getUrl = () =>{
-//     if(idCamera == camera._id){
-//         document.location.href = "/html/produit.html" //Renvoi sur la page produit
-//     }
-// }
-// //Work
-
 
 // let getUrlProduct = window.location.href
 // console.log("getUrlProduct", getUrlProduct)
--- js/appProduct.js
+++ js/appProduct.js
@@ -1,32 +1,32 @@
 //Appel API pour page produit
+//Appel URL
 let getUrlProduct = window.location.href //Obtiens l'URL actuel
-console.log("getUrlProduct", getUrlProduct)
-
 const urlProduct = new URL(getUrlProduct) //Permet d'obtenir les objet de l'url  
-console.log("urlProduct", urlProduct)
-
 const search_params = urlProduct.search //On récupère .search de l'URL
-console.log("search_params", search_params)
 
+//Appel de notre API
 if(search_params) {
     const id = search_params
-    console.log("id", id)
-
     fetch("http://localhost:3000/api/cameras" + id) //Rappel notre api + l'id de notre produit
     .then(result => result.json()) //Récupère le tableau json
     .then(cameras => { //Donne un nom au tableau json récupéré
-    console.log("cameras", cameras)
         const $cameraProduct = document.querySelector('#camera-product')
         cameras.forEach(camera => { //Permet de sélectionner chaque élément du tableau et de nommer chaque élement
             const cameraId = "?id=" + camera._id  
-            console.log("cameraId", cameraId)
             if(cameraId === id){ //Condition pour afficher l'élément avec le bon id
-                console.log("camera._id", camera._id)
+
+                //Mise en place liste déroulante pour les lentilles
+                const lenses = document.createElement("select")
+                let i = 0
+                for( i; i<camera.lenses.length; i++){
+                    const option = document.createElement("option")
+                    option.setAttribute("value", camera.lenses[i])                
+                    option.innerHTML = camera.lenses[i]
+                    lenses.appendChild(option)
+                }           
                 $cameraProduct.innerHTML += 
-                `<div id="camera-card" class="card col-lg-5 col-md-12 col-sm-12 col-12 mt-3 mb-3 border-dark shadow">
-                    <a href="../html/produit.html?id=${camera._id}">
-                        <div class="background-image-camera card-img-top" style="background-image: url(${camera.imageUrl})"></div>
-                    </a>
+                `<div id="camera-card" class="card col-10 mx-auto mt-3 mb-3 border-dark shadow">
+                    <div class="background-image-product card-img-top" style="background-image: url(${camera.imageUrl})"></div>
                     <div class="card-body">
                         <h5 class="card-title">${camera.name}</h5>
                         <p class="card-text">${camera.description}</p> 
@@ -34,21 +34,20 @@ if(search_params) {
                             <div class="input-group-prepend">
                                 <label class="input-group-text" for="inputGroupSelect01">Lentilles</label>
                             </div>
-                            <select class="custom-select col-lg-6 col-md-6 col-sm-6 col-12" id="inputGroupSelect01">
-                                <option selected value="1">${camera.lenses}</option>
-                                <option value="2">${camera.lenses}</option>
+                            <select class="custom-select col-lg-6 col-md-6 col-sm-6 col-12" id="inputGroupSelect01">    
+                                <option selected selected value="1">Sélectionner</option>  
+                                ${lenses.innerHTML}
                             </select>
                             <p id="camera-price" class="card-text col-lg-6 col-md-6 col-sm-6 col-12 mx-auto">${camera.price + " €"}</p>
                         </div>  
                         <a id="camera-buy"class="btn btn-secondary col-6 mx-auto mt-3" href="#" role="button">Ajouter au panier</a>
                     </div>   
                 </div>`
-                console.log("camera", camera)
-            }
-        console.log("camera", camera)   
+            } 
         })  
     })        
     .catch((error) => {
         console.log(error);
     })
 }
+
