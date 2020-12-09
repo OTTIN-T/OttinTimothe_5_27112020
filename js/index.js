7d00245e5113dfe34f5f52705613a7c62731b9cf
OTTIN <ottin.timothe@gmail.com>
2020-12-09 18:52:59 +0100

    Avancé CSS
    CSS header 99% 
    CSS page home 99%
    CSS footer 90%

--- js/index.js
+++ js/index.js
@@ -33,16 +33,16 @@ const lenseList = () => {
 //Notre template camera card
 const cameraCard = () => {
     $cameraList.innerHTML +=
-        `<div id="camera-card" class="card col-lg-5 col-md-12 col-sm-12 col-12 mt-3 mb-3 border-dark shadow">
+        `<div id="camera-card" class="card scale-down-center scale-up-center col-lg-5 col-md-12 col-sm-12 col-12 mt-3 mb-3 shadow">
         <a href="../html/product.html?id=${camera._id}">
             <div class="background-image-camera card-img-top" style="background-image: url(${camera.imageUrl})"></div>
         </a>
         <div class="card-body">
-            <h5 class="card-title">${camera.name}</h5>
+            <h3 class="card-title">${camera.name}</h3>
             <p class="card-text">${camera.description}</p> 
             <div class="col-12 mt-3">
                 <a href="../html/product.html?id=${camera._id}">
-                    <button type="button" id="camera-buy" class="add-to-cart btn btn-secondary col-6 mx-auto mt-1">Plus d'informations</button>
+                    <button type="button" id="camera-infos" class="add-to-cart  btn col-6 mx-auto mt-1">Plus d'informations</button>
                 </a>
             </div>               
         </div>   
