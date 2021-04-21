80d424213e2a214ef13d5a2d8faa910fc0058ca9
OTTIN <ottin.timothe@gmail.com>
2021-04-21 16:24:26 +0200

    V2.1.6
    Toujours le même problème

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
-let apiURL = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
-     ? "http://localhost:3000/api/cameras"
-     :"https://apporinoco.herokuapp.com/api/cameras"
+let apiURL = location.hostname === "localhost" || location.hostname === "127.0.0.1"
+    ? "http://localhost:3000"
+    : "https://apporinoco.herokuapp.com";
--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ let camera;
 let $cameraList = document.querySelector('#camera-list');
 
 //Appel de notre API
-fetch(`${apiURL}`)
+fetch(`${apiURL}/api/cameras`)
     .then(async result_ => { //On rend asynchrone notre fonction
         const result = await result_.json() //Le reste du code s'execute après l'execution de la promesse 
         result.forEach(result => {
@@ -21,7 +21,7 @@ fetch(`${apiURL}`)
 const cameraCard = () => {
     $cameraList.innerHTML +=
         `<div id="camera-card" class="card col-lg-5 col-md-12 col-sm-12 col-12 mt-3 mb-3 shadow">
-        <a href="OttinTimothe_5_27112020/html/product.html?id=${camera._id}">
+        <a href=../html/product.html?id=${camera._id}">
             <div class="background-image-camera card-img-top" style="background-image: url(${camera.imageUrl})"></div>
         </a>
         <div class="card-body">
